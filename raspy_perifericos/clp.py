be96e7e8d2d82f00225b3a3aad91777ee1c9d0af
Henrique da Silva <hpsilva@pm.me>
2024-02-26 13:21:28 -0300

    Add codigo para o presenciometro

--- raspy_perifericos/clp.py
+++ raspy_perifericos/clp.py
@@ -3,7 +3,7 @@ from instrumento import Termometro, Presenciometro
 
 class CLP:
     instrumentos = [
-        Termometro("Termometro_1", "28-031600c422ff"),
+        Termometro("Termometro_1", "28-031600c442ff"),
         # Termometro("Termometro_2", "codigo_aqui"),
         Presenciometro("Presenciometro_1", "codigo_aqui"),
     ]
